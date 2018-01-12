988c2beca816e385c30aa3a6aa6e47e80fadbf7e
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2018-01-12 18:07:56 +0100

    Amélioration du positionnement et javascript fonctionnel

--- js/tree.js
+++ js/tree.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    checkBtns.forEach(function(btn) {
-        btn.addEventListener("click", toggleActiveTodo)
+    
+    function setActive() {
+        this.classList.add("active")
+    }
+
+    var btn_click = document.querySelectorAll(".group-container-player");
+    btn_click.forEach(function(btn){
+        btn.addEventListener("click",setActive)
+
     })
 
 });
