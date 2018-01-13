d9080f66fbdfb617ca6f0b31a64fdaccf7b916a2
Nicolas notararigo <nicolas.notararigo@gmail.com>
2018-01-13 14:28:22 +0100

    Amélioration du JS. Arrivé du bouton 'winner' et de sa fonctionnalité.

--- js/tree.js
+++ js/tree.js
@@ -2,25 +2,68 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    function setActive() {
+    function setWin() {
         this.parentNode.classList.add("win");
-        if(this.parentNode.classList.contains("one")){
 
-            var id_arrow = this.parentNode.parentNode.getAttribute('id'), arrow_select = document.getElementById('arrow_' + id_arrow), arrow = arrow_select.childNodes;
 
-            for(var i = 0; i <= arrow.length; i++){
-                var test = arrow[i].classList.contains("group-container-line_top__left");
-                console.log("test")
+        var id_group = this.parentNode.parentNode.getAttribute('id'),
+            arrow_select = document.querySelectorAll('#arrow_' + id_group);
+
+        if (this.parentNode.classList.contains("one")) {
+
+            var player_select = document.querySelector('.group-container-player.two.group_' + id_group);
+            player_select.classList.add("loose");
+
+            var btn_delete_two = document.querySelector('.btn.two.win.group_' + id_group);
+            btn_delete_two.remove();
+
+            for (var i = 0; i <= arrow_select.length; i++) {
+                if (arrow_select[i].classList.contains("group-container-line_top__left") || arrow_select[i].classList.contains("group-container-line_top__right")) {
+                    arrow_select[i].classList.add("win");
+                    this.remove();
+                }else{
+                    arrow_select[i].classList.add("loose");
+                }
             }
-        }else{
-            console.log("chemin du bas");
+        } else {
+
+            var player_select_two = document.querySelector('.group-container-player.one.group_' + id_group);
+            player_select_two.classList.add("loose");
+
+            var btn_delete_one = document.querySelector('.btn.one.win.group_' + id_group);
+            btn_delete_one.remove();
+
+            for (var i = 0; i <= arrow_select.length; i++) {
+                if (arrow_select[i].classList.contains("group-container-line_bottom__left") || arrow_select[i].classList.contains("group-container-line_bottom__right")) {
+                    arrow_select[i].classList.add("win");
+                    this.remove();
+                }else{
+                    arrow_select[i].classList.add("loose");
+                }
+            }
         }
-        this.remove();
+    }
+
+    function setActive() {
+        this.parentNode.classList.toggle("active");
+        var _html = this.innerHTML, content = '<i class="fas fa-angle-up"></i>';
+        if(_html === content){
+            _html = '<i class="fas fa-angle-down"></i>';
+            console.log(_html);
+        }else{
+            _html = '<i class="fas fa-angle-up"></i>';
+            console.log(_html);
+        }
     }
 
     var btn_click = document.querySelectorAll(".win");
     btn_click.forEach(function(btn){
+        btn.addEventListener("click",setWin)
+    });
+
+    var btn_infos = document.querySelectorAll(".more");
+    btn_infos.forEach(function(btn){
         btn.addEventListener("click",setActive)
-    })
+    });
 
 });
