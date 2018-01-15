1efafe2020d7490be4af303bf994712ef7caf877
Nicolas notararigo <nicolas.notararigo@gmail.com>
2018-01-15 21:49:37 +0100

    Ajouts des containers des utilisateurs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-var nb_player = prompt("Please enter the player number (max player : 64)");
+var nb_player = prompt("Please enter the player number (max player : 32)");
 
 
     if (nb_player <= 4){
@@ -11,8 +11,6 @@ var nb_player = prompt("Please enter the player number (max player : 64)");
         var nb_container_player = 16, nb_round = 5;
     }else if (nb_player > 16 && nb_player <= 32){
         var nb_container_player = 32, nb_round = 6;
-    }else if (nb_player > 32 && nb_player <= 64){
-        var nb_container_player = 64, nb_round = 7;
     }
 
     create_container();
@@ -31,47 +29,35 @@ var nb_player = prompt("Please enter the player number (max player : 64)");
             var nb_group = document.querySelectorAll(".group").length;
         }
 
-        for (j = 1; j < (nb_container_player / 2) + 1; j++) {
+        for (k = 1; k <= (nb_container_player / 2); k++) {
             var arrow_container = "<div class=\"container-arrow\">\n" +
-                "            <div id=\"arrow_" + j + "\" class=\"group-container-line_top__left group_" + j + "\"></div>\n" +
-                "            <div id=\"arrow_" + j + "\" class=\"group-container-line_top__right group_" + j + "\"></div>\n" +
-                "            <div id=\"arrow_" + j + "\" class=\"group-container-line_bottom__right group_" + j + "\"></div>\n" +
-                "            <div id=\"arrow_" + j + "\" class=\"group-container-line_bottom__left group_" + j + "\"></div>\n" +
+                "            <div id=\"arrow_" + k + "\" class=\"group-container-line_top__left group_" + k + "\"></div>\n" +
+                "            <div id=\"arrow_" + k + "\" class=\"group-container-line_top__right group_" + k + "\"></div>\n" +
+                "            <div id=\"arrow_" + k + "\" class=\"group-container-line_bottom__right group_" + k + "\"></div>\n" +
+                "            <div id=\"arrow_" + k + "\" class=\"group-container-line_bottom__left group_" + k + "\"></div>\n" +
                 "        </div>";
             var arrow = document.querySelector(".arrow");
             arrow.insertAdjacentHTML("beforeend", arrow_container);
-            var container = document.querySelector(".container");
-            container.insertAdjacentHTML("beforeend", _round);
-        }
-        for (k = 1; k <= (nb_container_player / 2); k++) {
 
             var group = "<div id=\"g_" + k + "\" class=\"group group_id_" + k + "\">" +
-                "</div>";
-
-            var round = document.querySelector(".round");
+                "</div>",
+                round = document.querySelector(".round");
             round.insertAdjacentHTML("beforeend", group);
 
-            for(n = 1; n <= (nb_container_player)/4;n++){
-                var next_group = "<div id=\"g_" + n + "\" class=\"group group_id_"+ n +" group_"+ n +"\">" +
-                "</div>";
-                var next_round = document.querySelector("#r_2.round");
-                next_round.insertAdjacentHTML("beforeend", next_group);
-            }
 
             if (k == (nb_container_player / 2)) {
                 for (l = 1; l < 2; l++) {
                     for (m = 1; m <= (nb_container_player / 2); m++) {
                         var user1 = "<div id=\"player_" + l + "\" class=\"group-container-player one round_one group_" + m + "\">\n" +
-                            "                <span class=\"username\">username</span>\n" +
+                            "                <input type=\"text\" name=\"username\" placeholder='username' />\n" +
                             "                <button class=\"btn one win group_" + m + "\">Winner</button>\n" +
-                            "                <button class=\"btn active\"><i class=\"fas fa-angle-double-right\"></i></button>\n" +
+                            "                <button class=\"btn active\" alt='Follow the player' title='Follow the player'><i class=\"fas fa-angle-double-right\"></i></button>\n" +
                             "            </div>";
                         l++;
                         var user2 = "<div id=\"player_" + l + "\" class=\"group-container-player two round_one group_" + m + "\">\n" +
-                            "                <span class=\"username\">username</span>\n" +
+                            "                <input type=\"text\" name=\"username\" placeholder='username' />\n" +
                             "                <button class=\"btn two win group_" + m + "\">Winner</button>\n" +
-                            "                <button class=\"btn active\"><i class=\"fas fa-angle-double-right\"></i></button>\n" +
-                            "            </div>\n" +
+                            "                <button class=\"btn active\" alt='Follow the player' title='Follow the player'><i class=\"fas fa-angle-double-right\"></i></button>\n" +
                             "        </div>";
 
                         var group_add = document.querySelector(".group_id_" + m + "");
@@ -81,6 +67,58 @@ var nb_player = prompt("Please enter the player number (max player : 64)");
                 }
             }
 
+        }
+        for (n = 1; n <= (nb_container_player/4); n++){
+            var group_two = "<div id=\"g_" + n + "\" class=\"group group_round_two_" + n + "\">" +
+                "</div>",
+                next_round = document.querySelector("#r_2.round");
+                next_round.insertAdjacentHTML("beforeend", group_two);
+
+            if (n == (nb_container_player / 4)) {
+                for (o = 1; o <= (nb_container_player / 2); o++) {
+                    var user1_two = "<div id=\"\" class=\"group-container-player one round_two group_" + o + "\">\n" +
+                        "                <span class=\"username\">_____</span>\n" +
+                        "                <button class=\"btn one win group_" + o + "\">Winner</button>\n" +
+                        "                <button class=\"btn active\" alt='Follow the player' title='Follow the player'><i class=\"fas fa-angle-double-right\"></i></button>\n" +
+                        "            </div>";
+                    l++;
+                    var user2_two = "<div id=\"\" class=\"group-container-player two round_two group_" + o + "\">\n" +
+                        "                <span class=\"username\">_____</span>\n" +
+                        "                <button class=\"btn two win group_" + o + "\">Winner</button>\n" +
+                        "                <button class=\"btn active\" alt='Follow the player' title='Follow the player'><i class=\"fas fa-angle-double-right\"></i></button>\n" +
+                        "            </div>";
+
+                    var group_add_two = document.querySelector(".group.group_round_two_" + o + "");
+                    group_add_two.insertAdjacentHTML("beforeend", user1_two);
+                    group_add_two.insertAdjacentHTML("beforeend", user2_two);
+                }
+            }
+        }
+        for (p = 1; p <= (nb_container_player/8); p++){
+            var group_three = "<div id=\"g_" + p + "\" class=\"group group_round_two_" + p + "\">" +
+                "</div>",
+                next_round_three = document.querySelector("#r_3.round");
+            next_round_three.insertAdjacentHTML("beforeend", group_three);
+            if (n == (nb_container_player / 8)) {
+                for (q = 1; q <= (nb_container_player / 2); q++) {
+                    var user1_three = "<div id=\"\" class=\"group-container-player one round_two group_" + q + "\">\n" +
+                        "                <span class=\"username\">_____</span>\n" +
+                        "                <button class=\"btn one win group_" + q + "\">Winner</button>\n" +
+                        "                <button class=\"btn active\"><i class=\"fas fa-angle-double-right\"></i></button>\n" +
+                        "            </div>";
+                    l++;
+                    var user2_three = "<div id=\"" + l + "\" class=\"group-container-player two round_two group_" + q + "\">\n" +
+                        "                <span class=\"username\">_____</span>\n" +
+                        "                <button class=\"btn two win group_" + q + "\">Winner</button>\n" +
+                        "                <button class=\"btn active\"><i class=\"fas fa-angle-double-right\"></i></button>\n" +
+                        "            </div>\n" +
+                        "        </div>";
+
+                    var group_add_three = document.querySelector(".group.group_round_two_" + q + "");
+                    group_add_three.insertAdjacentHTML("beforeend", user1_three);
+                    group_add_three.insertAdjacentHTML("beforeend", user2_three);
+                }
+            }
         }
 
     }
